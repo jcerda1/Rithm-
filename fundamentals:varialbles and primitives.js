e4a28598e7ea50e778dc74adf9626ea1dab2f9ae
jcerda <jccer103@gmail.com>
2017-06-27 16:53:23 -0500

    aswers for variables and primitives sections

---
+++ fundamentals:varialbles and primitives.js
@@ -0,0 +1,62 @@
+//1. Create the following variables
+//   name, which is a string set to your current name
+//   dayOfBirth, which is a number set to the day of your birth month
+
+var name = 'Jeramiah';
+var dayOfBirth = 21;
+
+//2. See what happens when you have multiple variables of the same name. Which one takes precedence?
+
+  //  The varialble that is name last will be returned as the value. 
+
+var name = 'Me';
+var dayOfBirth = 11
+console.log(name, "should be 'Me'");
+console.log(dayOfBirth, 'should be 11')
+
+
+//3. Write some JavaScript that prompt's the user for their favorite color. 
+
+  //Once the user has submitted a favorite color, log that color to the console along with a friendly message.
+
+var usersFavColor = prompt('What is your favorite color?');
+
+console.log(usersFavColor)
+
+
+//4. Create a string that contains both single quotes and double quotes.
+
+var str = "Hello, sir. Please don't run!";
+
+
+//5. What is the difference between null and undefined?
+
+  //  undefined is any variable that is created in a program but not assaigned a value. 
+  // In contrast null is an a intentionally absence of data. 
+
+
+//6. What is NaN in JavaScript? What is the typeof NaN?
+
+  // NaN stands for not a number. When checking the type of NaN it returns number. 
+
+console.log(typeof NaN);
+
+
+// You can declare a variable by typing var thing;. What is the value of thing?
+
+  // When a variable is declared in this manner, it works as a place holder.
+  // the variable will remain undefined until given a value. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
